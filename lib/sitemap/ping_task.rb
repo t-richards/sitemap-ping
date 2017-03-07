adedd2fa76769ca9bdf29bfacac5962f06bc4f9d
Tom Richards <tom@tomrichards.net>
2017-03-07 18:25:21 -0500

    Rubocop

--- lib/sitemap/ping_task.rb
+++ lib/sitemap/ping_task.rb
@@ -3,14 +3,16 @@ require 'rake'
 require 'rake/tasklib'
 require 'uri'
 require 'yaml'
-#require 'sitemap/ping/version'
+
+require 'sitemap/ping/version'
 
 ENGINE_PING_BASEURLS = {
   google: 'https://www.google.com/webmasters/sitemaps/ping?sitemap=',
   bing: 'https://www.bing.com/webmaster/ping.aspx?siteMap='
-}
+}.freeze
 
 module Sitemap
+  # Rake task for initiating search engine sitmap "ping"s
   class PingTask < Rake::TaskLib
     attr_accessor :name
     attr_accessor :engines
@@ -22,7 +24,7 @@ module Sitemap
       @engines = [:google, :bing]
       @description = 'Inform search engines of updated sitemap contents'
       @deps = []
-      @sitemap_url = YAML.load_file('_config.yml')["url"] + "/sitemap.xml"
+      @sitemap_url = YAML.load_file('_config.yml')['url'] + '/sitemap.xml'
       yield self if block_given?
       define
     end
@@ -31,9 +33,8 @@ module Sitemap
       namespace :sitemap do
         desc @description
         task @name => Array(@deps) do
-          desired_engines = ENGINE_PING_BASEURLS.select { |k,v| @engines.include?(k) }
-          desired_engines.each do |k, v|
-            ping_sitemap(v, @sitemap_url)
+          ENGINE_PING_BASEURLS.each do |k, v|
+            ping_sitemap(v, @sitemap_url) if @engines.include?(k)
           end
         end
       end
